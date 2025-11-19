a6c8e8feddd135f84e0b88dbd51f3d1688ad7c35
miguelrtrombotto <miguelrtrombotto@gmail.com>
2025-11-19 12:55:50 -0300

    modifique base local y production.py para que funcione la url localhost:8000

--- proyecto_blog/blog/settings/local.py
+++ proyecto_blog/blog/settings/local.py
@@ -13,15 +13,38 @@ ALLOWED_HOSTS = []
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+# MODIFICADO: Cambiado de MySQL a SQLite para desarrollo sin servidor
 
 DATABASES = {
-    'default': 
-        {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'blog_db',
-            'USER': 'root',
-            'PASSWORD': '1234',
-            'HOST': 'localhost',
-            'PORT': '3306',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# from .base import *
+
+# # Quick-start development settings - unsuitable for production
+# # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-9hk^9t+dr^y@czh$4(-52uu=a3ajl_ut9bjfz6e(=_zm#9u=az'
+
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+
+# ALLOWED_HOSTS = []
+
+# # Database
+# # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+# DATABASES = {
+#     'default': 
+#         {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'NAME': 'blog_db',
+#             'USER': 'root',
+#             'PASSWORD': '1234',
+#             'HOST': 'localhost',
+#             'PORT': '3306',
+#         }
+# }
