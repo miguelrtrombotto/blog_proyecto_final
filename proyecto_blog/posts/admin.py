7ca75b8c9526e6dcab92e42b86ed5b554355535c
miguelrtrombotto <miguelrtrombotto@gmail.com>
2025-11-20 17:34:01 -0300

    Agregué modelo Post, registré en admin.py, instalé Pillow y creé superusuario

--- proyecto_blog/posts/admin.py
+++ proyecto_blog/posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'fecha_creacion', 'fecha_actualizacion']
+    search_fields = ['titulo', 'contenido']
+    list_filter = ['fecha_creacion', 'fecha_actualizacion']
+    readonly_fields = ['fecha_creacion', 'fecha_actualizacion']
--- proyecto_blog/posts/models.py
+++ proyecto_blog/posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    contenido = models.TextField()
+    imagen = models.ImageField(upload_to='posts/', null=True, blank=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.titulo
+    
+    class Meta:
+        ordering = ['-fecha_creacion']
